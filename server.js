cbcbebf3545f2777cd77d7de1c179587bb158e40
david <daviddelacruzmorales42@gmail.com>
2025-05-14 23:21:31 -0600

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'david esta probando desde main ' });
+  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'david esta probando desde main prueba 22' });
 });
 
 const PORT = process.env.PORT || 3000;
